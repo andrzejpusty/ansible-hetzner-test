062c84a5baebfa54a1aef6548e2ba09e5fed3a81
Andrzej Pusty <andrzej.pusty@gmail.com>
2026-03-24 21:04:34 +0100

    Initial commit

---
+++ roles/app/files/app.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+APP_NAME = os.getenv("APP_NAME", "devops-app-2")
+APP_ENV = os.getenv("APP_ENV", "dev")
+APP_VERSION = os.getenv("APP_VERSION", "1.0.0")
+
+@app.route("/")
+def home():
+    return f"Hello from {APP_NAME}!"
+
+@app.route("/health")
+def health():
+    return jsonify({"status": "ok"})
+
+@app.route("/info")
+def info():
+    return jsonify({
+        "app": APP_NAME,
+        "environment": APP_ENV,
+        "version": APP_VERSION,
+        "message": "Version 1.0.1 deployed using Ansible"
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
